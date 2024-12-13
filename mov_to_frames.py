a5353fb4069de3274de59e5f699e46a3d4719970
lukasha11 <lukas11ha11@gmail.com>
2024-12-13 21:57:49 +0100

    initial commit

---
+++ mov_to_frames.py
@@ -0,0 +1,21 @@
+import cv2
+import os
+
+
+output_folder = "test2"
+os.makedirs(output_folder, exist_ok=True)
+
+video_path = "C:/studia/magisterka/Praca magisterska/test/1.MOV"
+cap = cv2.VideoCapture(video_path)
+
+count = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_filename = os.path.join(output_folder, f"frame_{count:04d}.png")
+    cv2.imwrite(frame_filename, frame)
+    count += 1
+
+cap.release()
