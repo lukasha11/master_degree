8370078348cf18dc980bb4805390ff6c0ba2db89
lukasha11 <lukas11ha11@gmail.com>
2025-01-08 22:28:00 +0100

    adding xml reader

--- mov_to_frames.py
+++ mov_to_frames.py
@@ -1,21 +1,26 @@
 import cv2
 import os
 
-
-output_folder = "test2"
+output_folder = "test5_pokoj"
 os.makedirs(output_folder, exist_ok=True)
 
-video_path = "C:/studia/magisterka/Praca magisterska/test/1.MOV"
+video_path = "C:/studia/magisterka/Praca magisterska/test/4.MOV"
 cap = cv2.VideoCapture(video_path)
-#test git
+
 count = 0
+saved_count = 0
+
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    frame_filename = os.path.join(output_folder, f"frame_{count:04d}.png")
-    cv2.imwrite(frame_filename, frame)
+    if count % 2 == 0:
+        frame_filename = os.path.join(output_folder, f"frame_{saved_count:04d}.png")
+        cv2.imwrite(frame_filename, frame)
+        saved_count += 1
+
     count += 1
 
 cap.release()
+
---
+++ xml_reader.py
@@ -0,0 +1,6 @@
+import os
+import cv2
+
+frames_path = "frames"
+annotations_path = "annotations"
+
